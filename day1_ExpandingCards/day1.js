2a73b15b1f92e042538aad2867ef5b2affe4cef6
hye <hye0ah0@gmail.com>
2022-03-29 23:31:05 +0900

    ExpandingCards 복습
    imgs.forEach((img) => {
        img.addEventListener('click',() => {
            removeClasses()
            img.classList.add('move');
        })
    })
    
    function removeClasses(){
        imgs.forEach((img) => {
            img.classList.remove('move');
        })
    }

--- day1_ExpandingCards/day1.js
+++ day1_ExpandingCards/day1.js
@@ -16,3 +16,20 @@ function removeActiveClasses(){
         // console.log(panels);
     })
 }
+
+const imgs = document.querySelectorAll('.img');
+console.log (imgs);
+console.log(imgs.forEach);
+
+imgs.forEach((img) => {
+    img.addEventListener('click',() => {
+        removeClasses()
+        img.classList.add('move');
+    })
+})
+
+function removeClasses(){
+    imgs.forEach((img) => {
+        img.classList.remove('move');
+    })
+}
