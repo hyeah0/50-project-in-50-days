574a494d18630f9e627588642cf92237838e530a
hye <hye0ah0@gmail.com>
2022-03-31 23:18:14 +0900

    ProgressSteps 복습2

--- day2_ProgressSteps/day2.js
+++ day2_ProgressSteps/day2.js
@@ -129,3 +129,71 @@ function review1Update(){
         review1Next.disabled = false;
     }
 }
+
+// ===============review2=============== //
+// 1. 프로그래스바 next 클릭시 currentactive 숫자 +1
+// 순번 이상으로 next 클릭시 currentactive의 값은 최대 순번 값으로 한다
+// 2. 프로그래스바 prev 클릭시 currentactive 숫자 -1
+// 순번 이하으로 prev 클릭시 currentactive의 값은 최소 순번 값으로 한다
+// 3-1. next, prev 클릭시 순번에 색깔 변하게 하기
+// 3-2. next, prev 클릭시 progress bar 색깔 변하게 하기
+// 3-3. 최대 순번값이 되었을때 next 버튼 disabled 처리 , 
+//      최소 순번값이 되었을때 prev 버튼 disabled 처리
+
+const review2Progressbar = document.querySelector('.review2-progressbar');
+const review2Circles = document.querySelectorAll('.review2-circle');
+const review2Prev = document.querySelector('.review2-prev');
+const review2Next = document.querySelector('.review2-next');
+
+let review2CurrentAcctive = 1;
+
+review2Next.addEventListener('click', () => {
+    review2CurrentAcctive++;
+
+    if(review2CurrentAcctive > review2Circles.length){
+        review2CurrentAcctive = review2Circles.length     
+    }
+    // console.log(`review2CurrentAcctive : ${review2CurrentAcctive}`);
+    // console.log(`review2Circles.length : ${review2Circles.length}`);
+    review2MoveProgressBar();
+})
+
+review2Prev.addEventListener('click', () => {
+    review2CurrentAcctive--;
+
+    if(review2CurrentAcctive < 1){
+        review2CurrentAcctive = 1;
+    }
+    // console.log(`review2CurrentAcctive : ${review2CurrentAcctive}`);
+    review2MoveProgressBar();
+})
+
+function review2MoveProgressBar(){
+    // 3-1
+    review2Circles.forEach((review2Circle, review2Idx) => {
+        if(review2Idx < review2CurrentAcctive){
+            review2Circle.classList.add('review2-active');
+        }
+        else{
+            review2Circle.classList.remove('review2-active');
+        }
+    })
+
+    //3-2
+    const review2Actives = document.querySelectorAll('.review2-active');
+
+    review2Progressbar.style.width = (review2Actives.length -1)/(review2Circles.length -1) * 100 + '%';
+
+    //3-3
+
+    if(review2Actives.length === review2Circles.length){
+        review2Next.disabled = true;
+    }
+    else if(review2Actives.length === 1){
+        review2Prev.disabled = true;
+    }
+    else{
+        review2Next.disabled = false;
+        review2Prev.disabled = false;
+    }
+}
