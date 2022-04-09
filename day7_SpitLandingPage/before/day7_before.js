204c7bcf691fd839cb8eb6b9c4e98009ff484b30
hye <hye0ah0@gmail.com>
2022-04-09 22:59:51 +0900

    split landing page

---
+++ day7_SpitLandingPage/before/day7_before.js
@@ -0,0 +1,13 @@
+const boxes= document.querySelectorAll('.box');
+
+console.log(boxes);
+
+boxes.forEach(box => {
+    console.log(box);
+})
+
+// boxes.addEventListener('mouseover',active);
+
+// function active(){
+//     console.log("a");
+// }
